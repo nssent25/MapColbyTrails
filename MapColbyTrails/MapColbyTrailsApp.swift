65f2756f311cd40cb839d9ba0d8091b57316f642
nssent25 <nssent25@colby.edu>
2024-06-18 22:20:36 -0400

    Initial Commit

---
+++ MapColbyTrails/MapColbyTrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapColbyTrailsApp.swift
+//  MapColbyTrails
+//
+//  Created by Nithun S on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MapColbyTrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
